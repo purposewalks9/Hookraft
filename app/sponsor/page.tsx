a2c66df5e2dc3242a8a9009ff18cdb8e1c171d40
purposewalks9 <boypee71@gmail.com>
2026-04-04 21:09:30 +0100

    fix useSearchParams suspense boundary

--- app/sponsor/page.tsx
+++ app/sponsor/page.tsx
@@ -1,16 +1,20 @@
+import { Suspense } from "react";
 import { Footer } from "@/components/footer";
 import SiteHeader from "@/components/site-header";
 import { PricingSection } from "@/components/sponsor";
 import { getDocSchema } from "@/lib/doc";
 import { SponsorsCircle } from "@/components/sponsorcircle";
+
 const docSchema = await getDocSchema();
 
 export default function Home() {
   return (
-    <div className=" flex flex-col relative min-h-dvh pt-14 ">
+    <div className="flex flex-col relative min-h-dvh pt-14">
       <SiteHeader docSchema={docSchema} />
-        <SponsorsCircle />
-      <PricingSection />
+      <SponsorsCircle />
+      <Suspense fallback={null}>
+        <PricingSection />
+      </Suspense>
       <Footer />
     </div>
   );
