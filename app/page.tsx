52455b21c4019cf768ef6aa7531a9b103d89b4a7
purposewalks9 <boypee71@gmail.com>
2026-04-04 21:13:12 +0100

    fix suspense on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
+import { Suspense } from "react";
 import { Footer } from "@/components/footer";
 import { Hero } from "@/components/hero";
 import { HooksShowcase } from "@/components/showhooks";
 import SiteHeader from "@/components/site-header";
 import { PricingSection } from "@/components/sponsor";
 import { getDocSchema } from "@/lib/doc";
+
 const docSchema = await getDocSchema();
 
 export default function Home() {
   return (
-    <div className=" flex flex-col relative min-h-dvh pt-14 ">
+    <div className="flex flex-col relative min-h-dvh pt-14">
       <SiteHeader docSchema={docSchema} />
       <Hero />
       <HooksShowcase />
-      <PricingSection />
+      <Suspense fallback={null}>
+        <PricingSection />
+      </Suspense>
       <Footer />
     </div>
   );
